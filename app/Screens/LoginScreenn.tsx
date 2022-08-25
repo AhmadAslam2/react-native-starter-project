377cb7fbb08b9c874ec9efc52f745e019913d133
Ahmad Aslam <66871677+ahmadaslam1999@users.noreply.github.com>
2022-08-25 16:32:55 +0500

    Login Screen with Formik

--- app/Screens/LoginScreenn.tsx
+++ app/Screens/LoginScreenn.tsx
@@ -1,6 +1,7 @@
 import {Image, StyleSheet} from 'react-native';
-import React, {useState} from 'react';
+import React from 'react';
 import SafeAreaView from 'react-native-safe-area-view';
+import {Formik} from 'formik';
 
 import TextInputWithIcon from '../components/TextInputWithIcon';
 import CustomIcon from '../components/CustomIcon';
@@ -8,43 +9,48 @@ import colors from '../config/colors';
 import CustomButton from '../components/CustomButton';
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
   return (
     <SafeAreaView style={styles.container}>
       <Image source={require('../assests/logo.png')} style={styles.logo} />
-      <TextInputWithIcon
-        onChangeText={(value: React.SetStateAction<string>) => setEmail(value)}
-        placeholder="Enter your Email"
-        Icon={
-          <CustomIcon
-            name="mail-outline"
-            type="ionicon"
-            size={25}
-            color={colors.lightgrey}
-          />
-        }
-      />
-      <TextInputWithIcon
-        onChangeText={(value: React.SetStateAction<string>) =>
-          setPassword(value)
-        }
-        placeholder="Enter your Password"
-        secureTextEntry={true}
-        Icon={
-          <CustomIcon
-            name="lock-closed-outline"
-            type="ionicon"
-            size={25}
-            color={colors.lightgrey}
-          />
-        }
-      />
-      <CustomButton
-        color={colors.primary}
-        text="Login"
-        onPress={() => console.log(email, password)}
-      />
+
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={value => console.log(value)}>
+        {({handleChange, handleSubmit}) => (
+          <>
+            <TextInputWithIcon
+              onChangeText={handleChange('email')}
+              placeholder="Enter your Email"
+              Icon={
+                <CustomIcon
+                  name="mail-outline"
+                  type="ionicon"
+                  size={25}
+                  color={colors.lightgrey}
+                />
+              }
+            />
+            <TextInputWithIcon
+              onChangeText={handleChange('password')}
+              placeholder="Enter your Password"
+              secureTextEntry={true}
+              Icon={
+                <CustomIcon
+                  name="lock-closed-outline"
+                  type="ionicon"
+                  size={25}
+                  color={colors.lightgrey}
+                />
+              }
+            />
+            <CustomButton
+              color={colors.primary}
+              text="Login"
+              onPress={handleSubmit}
+            />
+          </>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 };
